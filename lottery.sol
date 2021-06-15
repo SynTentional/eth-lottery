8c01c0938e56a79c5f0c36f796b1ca1240c841fc
Christopher Mullins <cmullins318@gmail.com>
2021-06-15 13:12:01 -0700

    added enter funct for paying eth

--- lottery.sol
+++ lottery.sol
@@ -4,11 +4,19 @@ contract Lottery {
     // address data type with public access
     // access is public for front-end
     address public manager;
+    address[] public players;
 
     function Lottery() public{
         // whenever we create a contract
         // we receive the sender's address
         manager = msg.sender;
-
     }
+
+    // payable keyword is for funcs that send
+    // ether 
+    function enter() public payable {
+        // send along some amount of ETH
+        players.push(msg.sender);
+    }
+
 }
