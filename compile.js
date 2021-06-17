4b46551fa3430144acaa27893f72473874559477
Christopher Mullins <cmullins318@gmail.com>
2021-06-17 15:58:21 -0700

    updated compile.js

--- compile.js
+++ compile.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const solc = require('solc');
 
 
-const inboxPath = path.resolve(__dirname, 'contracts', 'inbox.sol')
+const lotteryPath = path.resolve(__dirname, 'contracts', 'lottery.sol')
 // read contents of file
-const source = fs.readFileSync(inboxPath, 'utf8');
+const source = fs.readFileSync(lotteryPath, 'utf8');
 
 //call compiler and access solidity code
-module.exports = solc.compile(source, 1).contracts[':Inbox'];
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
 //this will access the bytecode source (to add to network) and ABI
