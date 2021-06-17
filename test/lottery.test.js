b625a717056b6ff0d3381ccbd302e954ac5d1e2a
Christopher Mullins <cmullins318@gmail.com>
2021-06-17 16:12:22 -0700

    added another unit test

--- test/lottery.test.js
+++ test/lottery.test.js
@@ -16,8 +16,27 @@ beforeEach(async () => {
         .send({ from: accounts[0], gas: '1000000'});
 });
 
+//////////////////////////
+//        Tests         //
+//////////////////////////
+
 describe('Lottery Contract', () => {
     it('deploys a contract', () => {
         assert.ok(lottery.options.address);
+    });
+
+    it('allows one accounts to enter', async () => {
+        await lottery.methods.enter().send({
+            from: accounts[0],
+            value: web3.utils.toWei("0.02", "ether")
+        });
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+
+        assert.equal(accounts[0], players[0]);
+        assert.equal(1, players.length);
+
     });
 })
